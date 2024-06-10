e12c1d3cf5446f21dc9d09a504d189551b5ec5a0
anjali butola <168713054+butolaanjali@users.noreply.github.com>
2024-06-10 22:16:02 +0530

    file updated

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -137,7 +137,18 @@ function Home() {
               <h1 className="bga">Manjira Devi University</h1>
               <h1 className="bgb">The Best University in Uttarkashi, Uttarakhand</h1>
               <p className="bgc">
-                Welcome to Smt. Manjira Devi University Our university is housed in a magnificent, expansive building that offers a refreshing environment, naturally inspiring students to excel. We are dedicated to providing the highest quality education, facilitated by our experienced and highly educated faculty members. We proudly offer a full-time, two-year Bachelor of Education program, approved by the National Council for Teacher Education (NCTE) in Jaipur and affiliated with H.N.B. Garhwal University in Sri Nagar, Uttarakhand. Our faculty members are not only well-qualified but also deeply committed to their roles, ensuring that every student receives individual attention and support. At Smt. Manjira Devi University, we strive to cultivate an enriching educational experience that empowers students to achieve academic excellence and professional success.
+                Welcome to Smt. Manjira Devi University Our university is housed in a magnificent,
+                 expansive building that offers a refreshing environment, naturally inspiring
+                  students to excel. We are dedicated to providing the highest quality education,
+                   facilitated by our experienced and highly educated faculty members.
+                    We proudly offer a full-time, two-year Bachelor of Education program,
+                     approved by the National Council for Teacher Education (NCTE) in Jaipur 
+                     and affiliated with H.N.B. Garhwal University in Sri Nagar, Uttarakhand. 
+                     Our faculty members are not only well-qualified but also deeply committed 
+                     to their roles, ensuring that every student receives individual attention 
+                     and support. At Smt. Manjira Devi University, we strive to cultivate an 
+                     enriching educational experience that empowers students to achieve <center>academic
+                       excellence and professional success.</center>
               </p>
               
               <a href="/About" class="btn  bgd   " data-aos="fade-up">Know more</a>
