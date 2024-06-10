c4980fe5784327aa17116829991a546beac7fa5a
*Khushboo <*khushboo46negi@gmail.com*>
2024-06-10 22:11:06 +0530

    modified the page

--- src/Component/Program/Program.js
+++ src/Component/Program/Program.js
@@ -55,15 +55,36 @@ function Program() {
             </ul>
             <h3>School of Arts and Humanity</h3>
             <ul>
-              <li>Bachelor of Science</li>
-              <li>Bachelor of Hospital Administration (BHA)</li>
+              <li>BA (Hindi/Sanskrit/English/Sociology/Education/Master of Social Work)</li>
+              <li>BA (Economics/History/ Political Science/Library Science)</li>
+              <li>B.Ed</li>
+              <li>Ph.D.</li>
+              <li>MA</li>
             </ul>
-            <h3>School of Journalism & Liberal Arts</h3>
+            <h3>School of Yogic Science and Naturopathy</h3>
             <ul>
-              <li>Bachelor of Arts</li>
-              <li>Bachelor of Fine Arts (BFA)</li>
-              <li>B.Des. Fashion Design</li>
-              <li>B.Sc. Fashion Design</li>
+              <li>BNYS</li>
+              <li>BSC</li>
+              <li>M.A</li>
+              <li>Ph.D.</li>
+              <li>M.Sc</li>
+              <li>PGD-Yoga</li>
+            </ul>
+            <h3>School of Nursing,Paramedical and Allied Science</h3>
+            <ul>
+              <li>B.Pharma</li>
+              <li>B.Sc Nursing</li>
+              <li>BPT</li>
+              <li>ANM</li>
+              <li>GNM</li>
+              <li>D.Pharma</li>
+              <li>MPH</li>
+              <li>M.Sc.(Clinical Psychology)</li>
+            </ul>
+            <h3>School of Legal Studies</h3>
+            <ul>
+              <li>BALLB</li>
+              <li>LLB</li>
             </ul>
             
             <h3>School of Hotel Management & Tourism</h3>
