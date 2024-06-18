ab347162ae37a431d8e10e7512c3a5457d2b4074
*Khushboo <*khushboo46negi@gmail.com*>
2024-06-18 14:14:57 +0530

    add new folder and file

--- src/Component/Notification/Phdnotification.js
+++ src/Component/Notification/Phdnotification.js
@@ -1,25 +1,15 @@
 import React from 'react';
-import { Worker, Viewer } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
-import './Phdnotification.css';  // Ensure this file exists and has valid styles
+import './Phdnotification.css'; // Ensure this file has valid CSS styles
 
 const Phdnotification = () => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const pdfUrl = process.env.PUBLIC_URL + '/phd.pdf';
+  
 
   return (
-    <div className="pdf-viewer-container">
-      
-    
-      <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.10.377/pdf.worker.min.js">
-        <div className="pdf-viewer">
-          <Viewer fileUrl="/phd.pdf" plugins={[defaultLayoutPluginInstance]} />
-        </div>
-      </Worker>
-      <a href="/phd.pdf" className="btn bf4" download>
-        Download PDF
-      </a>
+    <div>
+      <div className="pdf-container">
+        <embed src={pdfUrl} type="application/pdf" width="100%" height="800px" />
+      </div>
     </div>
   );
 };
